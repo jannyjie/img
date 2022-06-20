c5487d1fbd99148ac1a11795926466b747f43d48
unknown <yuag1331@gmail.com>
2022-06-20 23:19:32 +0800

    build

--- src/utils/setMeta.js
+++ src/utils/setMeta.js
@@ -1,27 +1,107 @@
 import { useMeta } from "vue-meta";
-
-export default (params = {}) => {
+export default (meta = {}) => {
   useMeta({
-    title: params.title,
+    title: "Bla",
     meta: [
-      { vmid: "description", name: "description", content: params.description },
-      { vmid: "keywords", name: "keywords", content: params.keywords },
-      { vmid: "og:type", name: "og:type", content: "article" },
-      { vmid: "og:title", name: "og:title", content: params.title },
       {
-        vmid: "og:description",
-        name: "og:description",
-        content: params.description,
+        name: "title",
+        content: meta.title,
       },
       {
-        vmid: "og:url",
-        name: "og:url",
-        content: process.env.VUE_APP_BASE_URL + params.path,
+        name: "Description",
+        content: meta.description,
       },
       {
-        vmid: "og:image",
-        name: "og:image",
-        content: `${process.env.VUE_APP_BASE_URL}/images/og_image.jpg`,
+        name: "keywords",
+        content: meta.keywords,
+      },
+      {
+        property: "og:type",
+        content: "website",
+      },
+      {
+        property: "og:locale",
+        content: "zh_TW",
+      },
+      {
+        property: "og:title",
+        content: meta.title,
+      },
+      {
+        property: "og:url",
+        content: meta.url,
+      },
+      {
+        property: "og:image",
+        content: meta.image,
+      },
+      {
+        property: "og:description",
+        content: meta.description,
+      },
+      {
+        name: "google",
+        content: "notranslate",
+      },
+      {
+        name: "robots",
+        content: "max-image-preview:large, max-video-preview:-1",
+      },
+      {
+        "http-equiv": "Content-Language",
+        content: "zh-Hant",
+      },
+    ],
+    htmlAttrs: {
+      lang: "zh-Hant-TW",
+      class: "first-class",
+    },
+    twitter: {
+      title: meta.title,
+      description: meta.description,
+      keywords: meta.keywords,
+      "image:src": meta.image,
+    },
+    script: [
+      {
+        src: "application/ld+json",
+        json: [
+          {
+            "@context": "http://schema.org/",
+            "@type": "WebSite",
+            name: meta.title,
+            url: meta.url,
+            description: meta.description,
+            potentialAction: {
+              "@type": "SearchAction",
+              target:
+                "https://event.1111.com.tw/event22/22yAdmissionSystem/result?years=111&typeRef=1&schoolType=1&seachType=2&ticketNum={search_term_string}",
+              "query-input": "required name=search_term_string",
+            },
+          },
+          {
+            "@context": "http://schema.org",
+            "@type": "Organization",
+            name: "1111人力銀行",
+            url: "https://www.1111.com.tw",
+            logo: "https://www.1111.com.tw/images102/logo0504.png",
+            contactPoint: [
+              {
+                "@type": "ContactPoint",
+                telephone: "+886-02-8787-1111",
+                contactType: "customer service",
+                areaServed: ["TW"],
+                availableLanguage: ["Chinese"],
+              },
+            ],
+            sameAs: [
+              "https://www.facebook.com/1111jobbank",
+              "https://twitter.com/1111jobbanknews",
+              "https://www.youtube.com/user/1111JobBankNEWS",
+              "https://www.linkedin.com/company/1111-job-bank",
+            ],
+          },
+        ],
       },
     ],
   });
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,17 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "./",
-  transpileDependencies: ["vue-meta"],
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: ["/", "/about"],
+      useRenderEvent: true,
+      onlyProduction: true,
+
+      headless: false, // <- this could also be inside the customRendererConfig
+      customRendererConfig: {
+        args: ["--auto-open-devtools-for-tabs"],
+      },
+    },
+  },
 });
